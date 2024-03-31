c8349a124540456dd9bbd6e2d2f060eae43494eb
Jamal ouardi <ouardijamal87@gmail.com>
2024-03-31 10:51:45 +0000

    for EFF

--- app/src/main/java/com/example/m205_tp3_recyclerview/MainActivity.kt
+++ app/src/main/java/com/example/m205_tp3_recyclerview/MainActivity.kt
@@ -2,17 +2,22 @@ package com.example.m205_tp3_recyclerview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.ImageView
 import android.widget.ListView
 import android.widget.Spinner
+import android.widget.TextView
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.Volley
+import com.squareup.picasso.Picasso
 import org.json.JSONArray
 import org.json.JSONObject
 
+data class Movie(val id:Int, val name:String, val price:Double, val image:String)
 class MainActivity : AppCompatActivity() {
 
     var sa = JSONArray()
@@ -28,28 +33,54 @@ class MainActivity : AppCompatActivity() {
             { response ->
                 sa = response
 
-                val moviesList = ArrayList<String>()
+                val list1 = ArrayList<String>()
+
+                val list2 = ArrayList<Movie>()
 
 
 
                 for (i in 0 until sa.length()) {
                     val jsonObject: JSONObject = sa.getJSONObject(i)
-
+                    val id = jsonObject.getInt("id")
                     val name = jsonObject.getString("name")
                     val price = jsonObject.getDouble("price")
+                    val image = jsonObject.getString("image")
 
                     val displayed = name+" - "+price.toString()+" MAD"
 
+                    list1.add(displayed)
 
-                    moviesList.add(displayed)
+                    val movie = Movie(id,name,price,image)
+
+                    list2.add(movie)
 
                 }
 
                 val sp= findViewById<Spinner>(R.id.spinner)
-                val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, moviesList)
+                val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_dropdown_item, list1)
                 sp.adapter = adapter
+
+                sp.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                    override fun onItemSelected(parent: AdapterView<*>?, view: android.view.View?, position: Int, id: Long) {
+
+                        val selectedItem = list2[position]
+
+                        findViewById<TextView>(R.id.tvName).text = selectedItem.name
+                        findViewById<TextView>(R.id.tvPrice).text = selectedItem.price.toString()+" MAD"
+                        Picasso.get().load(selectedItem.image).into(findViewById<ImageView>(R.id.imageView))
+
+                    }
+
+                    override fun onNothingSelected(parent: AdapterView<*>?) {
+                        // Handle case when no item is selected
+                    }
+                }
+
+
+
+
             },
-            { error ->
+            { _ ->
 
             }
         )
